bba6ae4ddc32d71d3a700de35db1d4f868d1246c
Jacob Clouse <clouse.jp@gmail.com>
2023-03-11 17:43:51 -0500

    added bones to our file structure, added git ignore and steps to setup a virtual env

---
+++ Alt_N_Bit.py
@@ -0,0 +1,46 @@
+# Written by Luna D. and Jacob Clouse for ICSI 526 
+# Edited on Windows 10 - may need to be edited if you want to use on Linux/MacOS
+# Original Paper -> Alternative N-bit Key Data Encryption for Block Ciphers
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Importing Libraries / Modules 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+import datetime
+import os
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# Functions
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+# --- Function to print our logo ---
+def our_Logo():
+    print('Code designed, written & tested by:')
+    print('  |                                _ )            |         |          ')
+    print('  |      |   |  __ \    _` |       _ \ \          |   _` |  |  /   _ \ ')
+    print('  |      |   |  |   |  (   |      ( `  <      \   |  (   |    <    __/ ')
+    print(' _____| \__,_| _|  _| \__,_|     \___/\/     \___/  \__,_| _|\_\ \___| ')
+    print('Built for ICSI 526 - Spring 2023')
+
+
+# --- Function to Defang date time ---
+def defang_datetime():
+    current_datetime = f"_{datetime.datetime.now()}"
+
+    current_datetime = current_datetime.replace(":","_")
+    current_datetime = current_datetime.replace(".","-")
+    current_datetime = current_datetime.replace(" ","_")
+    
+    return current_datetime
+
+# --- Function that saves data to a file ---
+def write_out_data_to_file(output_file_name, data):
+    text_file = open(f"WriteOut_{output_file_name}.txt", "w")
+    yeahBoi = text_file.write(data)
+    text_file.close()
+
+
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+# MAIN 
+# =-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+our_Logo()
