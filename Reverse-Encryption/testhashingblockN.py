60c559d79e228d6229a648ecb701a3dd9f2de8a2
Jacob Clouse <clouse.jp@gmail.com>
2023-04-23 13:05:13 -0400

    found a way to use the encryption as a way to generate a 64 bit salt for our input

--- Reverse-Encryption/testhashingblockN.py
+++ Reverse-Encryption/testhashingblockN.py
@@ -1,7 +1,11 @@
 import hashlib
 
-n = 16
-h0 = hashlib.sha256(n.encode()).hexdigest()
+# we can use this to take the n input and act as if it is a bit of extra 'salt'
 
+n = input("Give me a number: ")
+# n = 16
+h0 = hashlib.sha256((str(n).encode())).hexdigest()
 
-print(len(n))
+print(f"h0: {h0}")
+print('\n')
+print(f"h0 len: {len(h0)}")
